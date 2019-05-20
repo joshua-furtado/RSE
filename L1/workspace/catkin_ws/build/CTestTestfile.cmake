ebfb4e2c5880b62084745f137a84bf1611602342
root <joshuafurtado2019@gmail.com>
2019-05-20 04:05:41 +0000

    Cloned TurtleBot gazebo package.

---
+++ L1/workspace/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /root/RSE/L1/workspace/catkin_ws/src
+# Build directory: /root/RSE/L1/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(turtlebot_simulator/turtlebot_gazebo)
+subdirs(turtlebot_simulator/turtlebot_simulator)
+subdirs(turtlebot_simulator/turtlebot_stage)
+subdirs(turtlebot_simulator/turtlebot_stdr)
---
+++ L1/workspace/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/root/RSE/L1/workspace/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/root/RSE/L1/workspace/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/root/RSE/L1/workspace/catkin_ws/devel;/opt/ros/kinetic" != "" else []
