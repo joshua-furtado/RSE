45ab1e2c7edce95483ff6c5bd6406171a5628cd4
root <joshuafurtado2019@gmail.com>
2019-06-15 19:58:19 +0000

    Clone turtlebot_simulator and turtlebot packages for gmapping

---
+++ L4/workspace/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/RSE/L4/workspace/catkin_ws/src
+# Build directory: /home/workspace/RSE/L4/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
