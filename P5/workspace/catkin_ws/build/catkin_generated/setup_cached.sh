4518b82ea630f4cfa70e8444e36e5be913f96185
root <joshuafurtado2019@gmail.com>
2019-06-23 20:57:01 +0000

    Create test_slam script

--- P5/workspace/catkin_ws/build/catkin_generated/setup_cached.sh
+++ P5/workspace/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -5,9 +5,11 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
+export TURTLEBOT_GAZEBO_MAP_FILE="/home/workspace/RSE/P5/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/maps/playground.yaml"
+export TURTLEBOT_GAZEBO_WORLD_FILE="/home/workspace/RSE/P5/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/worlds/playground.world"
+export TURTLEBOT_STAGE_MAP_FILE="/home/workspace/RSE/P5/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_stage/maps/maze.yaml"
+export TURTLEBOT_STAGE_WORLD_FILE="/home/workspace/RSE/P5/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_stage/maps/stage/maze.world"
+export TURTLEBOT_STDR_MAP_FILE="/home/workspace/RSE/P5/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_stdr/maps/sparse_obstacles.yaml"
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/workspace/RSE/P5/workspace/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/workspace/RSE/P5/workspace/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/RSE/P5/workspace/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/workspace/RSE/P5/workspace/catkin_ws/src:$ROS_PACKAGE_PATH"
---
+++ P5/workspace/catkin_ws/src/scripts/test_slam.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=worlds/UdacityOffice.world " &
+sleep 5
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_teleop keyboard_teleop.launch " &
+sleep 2
+xterm -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun gmapping slam_gmapping  " &
+sleep 3
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch "
+
+
