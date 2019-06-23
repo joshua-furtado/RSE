3c76249823aedc606a6c782d6eaf7dea7db4b039
root <joshuafurtado2019@gmail.com>
2019-06-23 06:40:35 +0000

    Clone necessary packages

---
+++ P5/workspace/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/RSE/P5/workspace/catkin_ws/src
+# Build directory: /home/workspace/RSE/P5/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot_interactions/turtlebot_dashboard")
+subdirs("turtlebot_interactions/turtlebot_interactions")
+subdirs("turtlebot_interactions/turtlebot_rviz_launchers")
+subdirs("turtlebot_interactions/turtlebot_interactive_markers")
