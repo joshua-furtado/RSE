43f4aad795730ebef9e1c12390561e372e9da4f3
root <joshuafurtado2019@gmail.com>
2019-06-24 05:55:09 +0000

    Generate map using gmapping SLAM

--- P5/workspace/catkin_ws/src/scripts/test_navigation.sh
+++ P5/workspace/catkin_ws/src/scripts/test_navigation.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/../turtlebot_simulator/turtlebot_gazebo/worlds/UdacityOffice.world " &
 sleep 5
-xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_teleop keyboard_teleop.launch " &
-sleep 2
-xterm -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo gmapping_demo.launch " &
-sleep 3
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo amcl_demo.launch " &
+sleep 5
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch "
 
 
