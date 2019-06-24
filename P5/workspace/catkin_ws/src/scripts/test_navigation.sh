d51e59657a6418a11d7cf18a34e4a99351e469db
root <joshuafurtado2019@gmail.com>
2019-06-24 06:10:30 +0000

    Test navigation using map generated

--- P5/workspace/catkin_ws/src/scripts/test_navigation.sh
+++ P5/workspace/catkin_ws/src/scripts/test_navigation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/../turtlebot_simulator/turtlebot_gazebo/worlds/UdacityOffice.world " &
 sleep 5
-xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo amcl_demo.launch " &
+xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../turtlebot_simulator/turtlebot_gazebo/maps/my_map7.yaml " &
 sleep 5
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch turtlebot_rviz_launchers view_navigation.launch "
 
